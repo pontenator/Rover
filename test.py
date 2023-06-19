91009ce17fed2211972c9123dbdf92f3671d51ac
pontenator <78854775+pontenator@users.noreply.github.com>
2023-06-19 16:03:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,113 +3,130 @@ from pylx16a.lx16a import *
 import time
 from time import sleep
 from sshkeyboard import listen_keyboard,stop_listening
+import keyboard
 LX16A.initialize("/dev/ttyUSB0")
 
-# FLAGS
+### FLAGS
 right_flag = False
 left_flag = False
-steering_flag = False
+steering_flag = True
+xbox_flag = False
 #mutexLock = False
 
+### Constants
+turn_radius = 0
+wheelbase = 0
+steering_angle = 0
+motorSpeedMax = 1000
+value = 0
+### VARIABLES
+#theta = 30
+#thetaFront = 120+theta
+#thetaBack = 0 #120-theta
+motorSpeed = 0
 
-# VARIABLES
-theta = 0
-motorSpeed = 500
+### Ackermann stearing
+#def calculate_steering_angle(turn_radius, wheelbase):
+#	 return math.atan(wheelbase / turn_radius)
 
-# KEYBOARD LISTENER
-def press(key):
-		#print(f"'{key}' pressed")
-	if key == 'w':
-		motorSpeed = 100
-		print("\nmotorSpeed=",motorSpeed)
+#def calculate_inner_wheel_angle(steering_angle, wheelbase):
+#	 return math.atan(2 * wheelbase * math.sin(steering_angle) / wheelbase)
 
-	if key == 'a':
-		theta = -45
-		print("\ntheta=",theta)
-		#stop_listening()
+#def calculate_outer_wheel_angle(steering_angle, wheelbase):
+#	 return math.atan(2 * wheelbase * math.sin(steering_angle) / wheelbase) + steering_angle
 
-	if key == 'd':
-		theta = 45
-		print("\ntheta=",theta)
+### KEYBOARD LISTENER
+async def press(key):
+#	print(f"'{key}' pressed")
+	global motorSpeed, theta
 
-	if key == 's':
-		motorSpeed = -100
-		print("\nmotorSpeed=",motorSpeed)
+	if key == 'right':
+		theta -= 20
+	elif key == "left":
+		theta += 20
+	elif key == "up":
+		motorSpeed += 100
+	elif key == "down":
+		motorSpeed -= 100
 
-listen_keyboard(on_press=press,)
+	print(f"motorSpeed: {motorSpeed}, theta: {theta}")
 
-# MANUAL INPUT
-#motorSpeed = 300
-#motorSpeed = input(f"motorspeed:\n")
-#motorSpeed = int(motorSpeed)
+	driveRF.motor_mode(-motorSpeed)
+	driveR.motor_mode(-motorSpeed)
+	driveRB.motor_mode(-motorSpeed)
 
-# INITIALIZE SERVOS
+	driveLF.motor_mode(motorSpeed)
+	driveL.motor_mode(motorSpeed)
+	driveLB.motor_mode(motorSpeed)
+
+	steerLF.move(140-theta)		# 150	_right	OK
+	steerRF.move(165-theta)		# 165	_right	OK
+	steerLB.move(140+theta)		# 140	_left	OK
+	steerRB.move(125+theta)		# 125	_left	OK
+#		if keyboard.press("a") == True:
+#				print(f"pressed a")
+
+		#theta = theta-1
+
+
+
+
+async def release(key):
+#	print(f"'{key}' released")
+	pass
+
+
+
+
+#print('yo')
+
+
+# Initialize servos
 try:
-	servo1 = LX16A(1)  	# right front drive
-	servo2 = LX16A(2)  	# right front steering
-	servo3 = LX16A(3)  	# side drive
-	servo4 = LX16A(4) 	# right back steering
-	servo5 = LX16A(5)  	# right back drive
-	servo6 = LX16A(6)  	# left front drive
-	servo7 = LX16A(7)  	# left front steering
-	servo8 = LX16A(8)  	# left drive
-	servo9 = LX16A(9)  	# right back steering
-	servo10 = LX16A(10) 	# right back drive
-	#servo11 = LX16A(11) 	# backup
+	driveRF = LX16A(1)
+	steerRF = LX16A(2)
+	driveR = LX16A(3)
+	steerRB = LX16A(4)
+	driveRB = LX16A(5)
+
+	driveLF = LX16A(6)
+	steerLF = LX16A(7)
+	driveL = LX16A(8)
+	steerLB = LX16A(9)
+	driveLB = LX16A(10)
+
+	steerRF.set_angle_limits(0,240)
+	steerRB.set_angle_limits(0,240)
+	steerLF.set_angle_limits(0,240)
+	steerLB.set_angle_limits(0,240)
 
 except ServoTimeoutError as e:
 	print(f"Servo {e.id_} is not responding. Exiting...")
 	quit()
 
 
-# START YO ENGINES
-#try:
-while True:
-	if right_flag == True:
-		servo1.motor_mode(motorSpeed)
-		servo3.motor_mode(motorSpeed)
-		servo5.motor_mode(motorSpeed)
 
-	if left_flag == True:
-		servo6.motor_mode(motorSpeed)
-		servo8.motor_mode(motorSpeed)
-		servo10.motor_mode(motorSpeed)
-# INTERRUPT
+theta = 0
+thetaFront = 120
+thetaBack = 140 #120-theta
+
+### Main loop
 try:
-    while True:
-        sleep(0.5)
+	listen_keyboard(
+		on_press=press,
+		on_release=release,
+	)
 
+### INTERRUPT
 except KeyboardInterrupt:
 	print('\nInterrupted!')
 
 	# Stop all servos
-	servo1.motor_mode(0)
-	servo3.motor_mode(0)
-	servo5.motor_mode(0)
-	servo6.motor_mode(0)
-	servo8.motor_mode(0)
-	servo10.motor_mode(0)
-	quit()
+	driveRF.motor_mode(0)
+	driveR.motor_mode(0)
+	driveRB.motor_mode(0)
 
-
-
-
-# STEERING
-#theta= 0
-#theta_right = 120-theta
-#theta_left = 120+theta
-
-while True:
-	# CONSTRAIN SERVO ANGLES
-	servo2.set_angle_limits(0,240)
-        servo4.set_angle_limits(0,240)
-        servo7.set_angle_limits(0,240)
-        servo9.set_angle_limits(0,240)
-	#servo11.set_angle_limits(0,240)		# backup servo
-
-	if steering_flag == True
-		servo2.move(theta_right)
-        	servo4.move(theta_right)
-        	servo7.move(theta_left)
-        	servo9.move(theta_left)
-		#servo11.move(theta_right)
+	driveLF.motor_mode(0)
+	driveL.motor_mode(0)
+	driveLB.motor_mode(0)
+#	quit()
